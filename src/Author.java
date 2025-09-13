f215a331b492e622a0b82b892e289e1267840026
IgorKaisarov <Igo444@mail.ru>
2025-09-13 14:56:26 +0600

    Home Work Object Methods

---
+++ src/Author.java
@@ -0,0 +1,40 @@
+public class Author {
+
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Author author = (Author) obj;
+        return firstName.equals(author.firstName) && lastName.equals(author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(firstName, lastName);
+
+    }
+
+}
+
+
